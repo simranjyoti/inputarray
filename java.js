baf126e5843e986137f9e45fe88f55fdd56f72f5
simranjyoti <152837624+simranjyoti@users.noreply.github.com>
2023-12-21 10:29:09 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+var numberArr = [23,44,55,66,77,43];
+var  len = numberArr.length;
+var i;
+var sum=0;
+
+for(i=0;i<len;i++){
+    document.write('va lue'+i+'is :'+numberArr[i]+'<br>');
+    // sum=sum+numberArr[i];
+    // sum+= numberArr[i];
+}
+// document.write('Sum is: '+sum);
